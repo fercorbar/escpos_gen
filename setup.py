e191923a563286dac7e4f63922c01cae31729d8c
Fernando Córdova <fernando@domus.mx>
2021-11-19 00:10:47 -0600

    added python versions to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
